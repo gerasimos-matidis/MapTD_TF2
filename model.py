81c95672be74cece9d34995bd18edd9b3fb76109
Gerasimos Matidis <g.matidis@uniwa.gr>
2022-10-24 16:51:10 +0300

    The MapTD model is ready!

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import keras
 from keras.applications.resnet import ResNet50
-from keras.utils import plot_model
 
 UNPOOL_LAYERS_FILTERS = [128, 64, 32]
 SKIP_LAYERS = ['conv4_block6_out', 'conv3_block4_out', 'conv2_block3_out']
@@ -13,32 +12,50 @@ BN_PARAMS = {
 }
 
 def resize_bln(inputs, resize_factor=2):
-    return tf.image.resize(inputs, size=[tf.shape(inputs)[1] * resize_factor, tf.shape(inputs)[2] * resize_factor])
+    return tf.image.resize(inputs, size=[tf.shape(inputs)[1] * resize_factor, 
+        tf.shape(inputs)[2] * resize_factor])
 
-def unpool_block(model, x, conv_filters = None, skip_layer_name=None, index=None, bn_parameters=None, conv_regularizer=None):
-    x = keras.layers.Lambda(resize_bln, name=f'unpool_{index}')(x)
-    x = keras.layers.concatenate([x, model.get_layer(skip_layer_name).output], axis=-1)
-    x = keras.layers.Conv2D(conv_filters, (1, 1), padding='same', kernel_regularizer=conv_regularizer)(x)
+def unpool_block(model, x, conv_filters = None, skip_layer_name=None, 
+        index=None, bn_parameters=None, conv_regularizer=None):
+    x = keras.layers.Lambda(resize_bln, name=f'unpool_{index + 1}')(x)
+    x = keras.layers.concatenate([x, model.get_layer(skip_layer_name).output], 
+        axis=-1, name=f'concatenate_{index + 1}')
+    x = keras.layers.Conv2D(conv_filters, 1, padding='same', 
+        kernel_regularizer=conv_regularizer)(x)
     x = keras.layers.BatchNormalization(**bn_parameters)(x)
     x = keras.layers.Activation('relu')(x)
-    x = keras.layers.Conv2D(conv_filters, (3, 3), padding='same', kernel_regularizer=conv_regularizer)(x)
+    x = keras.layers.Conv2D(conv_filters, 3, padding='same', 
+        kernel_regularizer=conv_regularizer)(x)
     x = keras.layers.BatchNormalization(**bn_parameters)(x)
     x = keras.layers.Activation('relu')(x)
 
     return x
 
-def maptd_model(input_shape=(512, 512, 3)):
-    inputs = keras.layers.Input(shape=input_shape)
+def maptd_model(input_size=512):
+    inputs = keras.layers.Input(shape=(input_size, input_size, 3))
     resnet_backbone = ResNet50(input_tensor=inputs, include_top=False)
     x = resnet_backbone.get_layer(index=-1).output
     for i in range(3):
-        x = unpool_block(resnet_backbone, x, conv_filters=UNPOOL_LAYERS_FILTERS[i], skip_layer_name=SKIP_LAYERS[i], bn_parameters=BN_PARAMS, conv_regularizer=CONV_REGULARIZER)
-    x = keras.layers.Conv2D(32, (3, 3), padding='same', kernel_regularizer=CONV_REGULARIZER)(x)
+        x = unpool_block(resnet_backbone, x, 
+            conv_filters=UNPOOL_LAYERS_FILTERS[i], 
+            skip_layer_name=SKIP_LAYERS[i], bn_parameters=BN_PARAMS, 
+            index=i,
+            conv_regularizer=CONV_REGULARIZER)
+    x = keras.layers.Conv2D(32, 3, padding='same', 
+        kernel_regularizer=CONV_REGULARIZER)(x)
     x = keras.layers.BatchNormalization(**BN_PARAMS)(x)
     x = keras.layers.Activation('relu')(x)
 
+    prediction = keras.layers.Conv2D(1, 1, activation='sigmoid', 
+        name='prediction_vector')(x)
+    rbox_scale = input_size
+    rbox_vector = rbox_scale * keras.layers.Conv2D(4, 1, activation='sigmoid', 
+        name='region_box_vector')(x)
+    angle_factor = 2 #NOTE: Factor to multiply by the angle vector, because 
+    # the codomain of tf.atan belongs to (-pi, pi) (see next line)
+    angle = angle_factor * keras.layers.Conv2D(1, 1, activation=tf.atan, 
+        name='angle_vector')(x)
+    geometry_vector = keras.layers.Concatenate()([rbox_vector, angle])
 
-
-
-
-
+    return keras.Model(inputs=inputs, outputs=[prediction, geometry_vector])
+    
